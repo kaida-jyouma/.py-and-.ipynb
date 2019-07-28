86dd3bd8830e0ba23f50ea91d55de60583e1110c
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2019-07-28 23:28:00 +0900

    Update CAmodel-2d.py

--- CAmodel-2d.py
+++ CAmodel-2d.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import math as mt
 import random as rdm
 import sys
@@ -7,16 +8,17 @@ nn = chr(9633)
 fi = chr(9635)
 def check(list_name, x, y): # list_name => 2次元配列
 	# return format => [set_bool, top_bool, right_bool, bottom_bool, left_bool]
+	retl = [False, False, False, False, False]
 	if list_name[x][y] == lv: # 指定の座標がlvであれば
-		if x == 0:
+		retl[0] = True
+		if x == 0: # xがリストの最初の列を指定した時
+            
+		elif x == (len(list_name) - 1): # xがリストの最後の列を指定した時
+            
+		elif y == 0: # yがリストの最初の行を指定した時
 			
-		elif x == (len(list_name) - 1):
+		elif y == (len(list_name) - 1): # yがリストの最後の行を指定した時
 			
-		elif y == 0:
-			
-		elif y == (len(list_name) - 1):
-			
-		else:
+		else: #その他
 			
 	else:
-		return([False, False, False, False, False])
