9eec483c92a8a0a7bffbdd045eec65cb4931d444
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2019-07-28 23:11:23 +0900

    Create CAmodel-2d.py

---
+++ CAmodel-2d.py
@@ -0,0 +1,13 @@
+import math as mt
+import random as rdm
+import sys
+import time as tm
+lv = chr(9632)
+nn = chr(9633)
+fi = chr(9635)
+def check(list_name, x, y): # list_name => 2次元配列
+	# return format => [set_bool, top_bool, right_bool, bottom_bool, left_bool]
+	if list_name[x][y] == lv: # 指定の座標がlvであれば
+		
+	else:
+		return([False, False, False, False, False])
