1e289b6e95f1444c1aff5003aa406e85416f8e3d
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2022-02-14 15:40:35 +0900

    Create max_prime.py

---
+++ max_prime.py
@@ -0,0 +1,31 @@
+def fact(n):
+    # 引用: https://qiita.com/snow67675476/items/e87ddb9285e27ea555f8
+    arr = []
+    temp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if temp%i==0:
+            cnt=0
+            while temp%i==0:
+                cnt+=1
+                temp //= i
+            arr.append([i, cnt])
+
+    if temp!=1:
+        arr.append([temp, 1])
+
+    if arr==[]:
+        arr.append([n, 1])
+
+    return arr
+
+# ここから
+a = int(input())
+for i in range(2, a + 1):
+    print(fact(i), fact(i)[-1][0])
+    fl = fact(i)
+    k = 1 # 仮置き
+
+    for j in range(len(fl)):
+        k *= (fl[j][0] ** (fl[j][1] + 1) - 1) / (fl[j][0] - 1)
+
+    print(i,fl[-1][0], int(k))
