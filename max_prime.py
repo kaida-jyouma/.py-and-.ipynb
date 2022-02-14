ca385975e6f8ed626f2b3f24343f46b6c31eec1c
kaida-jyouma <53356184+kaida-jyouma@users.noreply.github.com>
2022-02-14 16:31:08 +0900

    Update max_prime.py

--- max_prime.py
+++ max_prime.py
@@ -27,6 +27,7 @@ if least >= up:
     import sys
     sys.exit("最大値と最小値の差を1以上にしてください")
 
+write = input("書き込みの有無を選択してください (y/n): ")
 
 least = int(least)
 up = int(up)
@@ -35,6 +36,22 @@ print()
 print("min:", least)
 print("max:", up)
 print()
+
+
+if write == "y":
+    from google.colab import auth
+    from oauth2client.client import GoogleCredentials
+    import gspread
+    # 認証処理
+    auth.authenticate_user()
+    gc = gspread.authorize(GoogleCredentials.get_application_default()) # この辺変えたら死ぬんで変えないように。
+    # 'number'というスプレッドシートの先頭ワークシートをオープン
+    worksheet = gc.open('number').get_worksheet(0) # ←要するにファイル名"number"は任意に変えられるから自由に変えて。スプレッドシートは先に作っておいてください。
+
+
+
+# 注釈: 認証処理は一度限りのようなのでアカウントを間違えないように･･････。
+
 
 
 for i in range(least, up + 1):
@@ -46,3 +63,8 @@ for i in range(least, up + 1):
         k *= (fl[j][0] ** (fl[j][1] + 1) - 1) / (fl[j][0] - 1)
 
     print(i, fl[-1][0], int(k))
+    if write == "y":
+        # worksheet.update_cell(3, 5, 1000) ←言ってることは、「E3セルを"1000"に書きかえる」ってことなのでいい感じに調整してください。
+        worksheet.update_cell(i - least + 1, 1, i)
+        worksheet.update_cell(i - least + 1, 2, fl[-1][0])
+        worksheet.update_cell(i - least + 1, 3, int(k))
